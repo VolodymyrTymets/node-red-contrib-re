bbadaa5cb8881644a10d498afa2154b9a42e0611
Orest Andrusyshyn <Fallen_2006@ukr.net>
2018-06-20 15:57:28 +0300

    fixed node-input-at

--- pallets/time-based-trigger/src/PalletManager.js
+++ pallets/time-based-trigger/src/PalletManager.js
@@ -9,9 +9,41 @@ class PalletManager {
     this._self = node;
 
     this._self.interval = palletConfig.interval;
-    this._self.at = palletConfig.at;
-
+    this._self.value = palletConfig.value;
+    this._getDelayInMilliseconds = this._getDelayInMilliseconds.bind(this._self);
+    this._getNextWeekDay = this._getNextWeekDay.bind(this._self);
     this.onInput = this.onInput.bind(this._self);
+  }
+
+  _getNextWeekDay (startDate, dayOfWeek){
+    const dayOffset = dayOfWeek > startDate.getDay()
+        ? dayOfWeek - startDate.getDay()
+        : dayOfWeek - startDate.getDay() + 7;
+
+    startDate.setDate(startDate.getDate() + dayOffset);
+
+    return startDate;
+  }
+
+  _getDelayInMilliseconds(interval, value) {
+      const now = new Date();
+      const intervalMap = {
+          'monthly': 30 * 24 * 60 *1000,
+          'weekly': () => {
+              switch (value) {
+                  case 'Monday': return this._getNextWeekDay(new Date(),1).getTime() - now; // 0 = Sunday, 1 = Monday - new Date() = miliseconds left
+                  case 'Tuesday': return this._getNextWeekDay(new Date(),2).getTime() - now;
+                  case 'Wednesday': return this._getNextWeekDay(new Date(),3).getTime() - now;
+                  case 'Thursday': return this._getNextWeekDay(new Date(),4).getTime() - now;
+                  case 'Friday': return this._getNextWeekDay(new Date(),5).getTime() - now;
+                  case 'Saturday': return this._getNextWeekDay(new Date(),6).getTime() - now;
+                  case 'Sunday': return this._getNextWeekDay(new Date(),7).getTime() - now;
+              }
+          },
+          'daily': 24 * 60 * 1000,
+      };
+
+      return isNaN(value) ? intervalMap[interval] : ((intervalMap[interval] || 0) * value);
   }
 
   /**
@@ -23,24 +55,12 @@ class PalletManager {
    *      this.send(msg);
    * **/
   onInput(msg) {
-      console.log('ON INPUT');
       try {
           const {interval, atInput} = this;
+          const delay = this._getDelayInMilliseconds(interval, value);
 
-          const trigger = {interval, atInput};
-
-          // todo: extend logic here
-          // if(payloadEvent &&
-          //   payloadEvent.namespace === namespace &&
-          //   payloadEvent.entity === entity) {
-
-          _.extend(msg.payload, {trigger});
-          console.log(msg.payload);
+          _.extend(msg.payload, {delay});
           this.send(msg);
-          // } else {
-          //   this.warn('no trigger was found');
-          // }
-
       } catch (error) {
           this.error(error);
       }
